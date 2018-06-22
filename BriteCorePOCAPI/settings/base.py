d6104c72a9fd430c0ff3fbcf3f28524e02d871ea
Mahesh Bodas <36446088+MaheshBodas@users.noreply.github.com>
2018-06-22 16:44:29 +0530

    Add files via upload
    Changes for 
    1. To include WhiteNoise as a Middleware.
    2. To specify Database path using database url variable 
    3. SETTINGS config in base.py

--- BriteCorePOCAPI/settings/base.py
+++ BriteCorePOCAPI/settings/base.py
@@ -152,3 +152,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- BriteCorePOCAPI/settings/production.py
+++ BriteCorePOCAPI/settings/production.py
@@ -1,10 +1,9 @@
 from .base import *
+import dj_database_url
 
 ENVIRONMENT = 'production'
 DEBUG = True
+ALLOWED_HOSTS = ['*']
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'dev_db.sqlite3'),
-    }
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }

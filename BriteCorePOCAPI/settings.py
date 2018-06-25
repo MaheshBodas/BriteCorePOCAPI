f2b87d110018ef192bae3e087e324f79a9783dd5
Mahesh Bodas <36446088+MaheshBodas@users.noreply.github.com>
2018-06-25 19:46:21 +0530

    Add files via upload
    Removed django_heroku reference.

--- BriteCorePOCAPI/settings.py
+++ BriteCorePOCAPI/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -158,7 +158,7 @@ DATABASES = {
     }
 }
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
